4f438cdfac2f80c87a85928859ab04a2924499f0
Dana Adeyemi <dana.adeyemi@horizon.dev>
2026-02-10 08:37:57 +0300

    feat(auth): implement websocket events

---
+++ src/shared/components/ui/logo.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { useTheme } from '../theme-provider';
+import logoOnlyLight from '@/assets/logos/logo-only-light.png';
+import logoOnlyDark from '@/assets/logos/logo-only-dark.png';
+import logoTextBottomLight from '@/assets/logos/logo-text-bottom-light.png';
