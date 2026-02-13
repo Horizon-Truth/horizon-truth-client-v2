827a9e5dd4ab0f4eddb7f9e4537b146b5a05de03
Abduljebar Sani <Abduljebar49@gmail.com>
2026-02-13 16:18:53 +0300

    chore: Update API base URL to `https://api-dev.horizontruth.org` in environment files and add a runtime check for `VITE_API_URL` in the API service.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/auth.store';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log("API _ URL : ",API_URL)
+if (!API_URL) {
+  throw new Error("VITE_API_URL is not defined");
+}
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
