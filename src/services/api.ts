e08aa0ceb8788fa532996aa86a4bb2d203227798
Yuki Tanaka <yuki.tanaka@horizon.dev>
2026-04-12 10:32:38 +0300

    feat(organizations): implement calendar view

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+import { useAuthStore } from '../store/auth.store';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log("API _ URL : ",API_URL)
+if (!API_URL) {
+  throw new Error("VITE_API_URL is not defined");
+}
