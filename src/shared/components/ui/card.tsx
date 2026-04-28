ad1621ca0673e34de9a5d65eb5a73f73277f45a8
Dana Adeyemi <dana.adeyemi@horizon.dev>
2026-04-28 15:57:22 +0300

    test(shared): add tests for builder

--- src/shared/components/ui/card.tsx
+++ src/shared/components/ui/card.tsx
@@ -24,3 +24,13 @@ const CardHeader = React.forwardRef<
   <div
     ref={ref}
     className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+))
+CardHeader.displayName = "CardHeader"
+
+const CardTitle = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
