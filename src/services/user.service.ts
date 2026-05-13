9bb7f0b938672b42b4c364eb248c24843144b16b
Yuki Tanaka <yuki.tanaka@horizon.dev>
2026-05-13 10:02:35 +0300

    refactor(misc): simplify middleware

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,3 +4,16 @@ export interface UpdateProfileData {
     fullName?: string;
     username?: string;
     phone?: string;
+    preferredLanguage?: string;
+}
+
+export const userService = {
+    async getProfile() {
+        const response = await api.get('/users/me');
+        return response.data;
+    },
+
+    async updateProfile(data: UpdateProfileData) {
+        const response = await api.put('/users/me/profile', data);
+        return response.data;
+    },
