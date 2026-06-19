e5b58dc2207cc7962990f74d92636956f774b1c2
Abduljebar Sani <Abduljebar49@gmail.com>
2026-06-19 11:16:54 +0300

    refactor(resources): simplify metric

--- src/assets/photos/index.ts
+++ src/assets/photos/index.ts
@@ -3,3 +3,6 @@ import AbJPhoto from './AbduljebarS.jpg';
 import AbdA from './AbdurehmanA.jpg';
 import AjaM from './AjaibMoh.jpg';
 import BikEl from './BilkesEl.jpg';
+import GetIM from './Getahun.jpg';
+import MomHas from './mohammedHass.jpg';
+import MohmIb from './MohammedIb.jpg';
